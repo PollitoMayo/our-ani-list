6642e306ec66d6ff51cf21e4a24500c6465bfa13
Jorge Verdugo <jorgeverdugoch@gmail.com>
2022-07-16 18:50:55 -0400

    Admin helper

--- app/src/main/java/cl/exdev/ouranilist/MainActivity.java
+++ app/src/main/java/cl/exdev/ouranilist/MainActivity.java
@@ -2,7 +2,9 @@ package cl.exdev.ouranilist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void singUpUser(View view) {
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "ouranilist", null, 1);
+        SQLiteDatabase db = admin.getWritableDatabase();
+
+
     }
 }
