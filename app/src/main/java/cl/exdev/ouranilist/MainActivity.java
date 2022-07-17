98b6543cc3e271a0761f3b0bf66acc6d8b7937b8
Jorge Verdugo <jorgeverdugoch@gmail.com>
2022-07-16 22:07:32 -0400

    Basic list

--- app/src/main/java/cl/exdev/ouranilist/MainActivity.java
+++ app/src/main/java/cl/exdev/ouranilist/MainActivity.java
@@ -8,13 +8,24 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView _animesList;
+
+    private String elements[] = {"uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa", "uwu", "awa"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        _animesList = (ListView) findViewById(R.id.animes_list);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, elements);
+        _animesList.setAdapter(adapter);
     }
 }
